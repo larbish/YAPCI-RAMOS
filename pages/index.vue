7da5c7b8f403cb4108d0f5f9520e3eb851615e86
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-08-20 12:44:25 +0530

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-[100dvh] w-full relative" @click="handleClick">
-        <video preload="metadata" src="@/assets/videos/01-Yapci-parto-de-mi.mp4" muted autoplay loop
+        <video preload="metadata" src="/videos/01-Yapci-parto-de-mi.mp4" muted autoplay loop
             class="h-full w-full object-cover" playsinline></video>
 
 
@@ -8,7 +8,7 @@
         <div v-if="overlayState == 0 || overlayState == 1" class="absolute top-0 w-full h-full bg-black/50 overlay"
             ref="overlay">
             <div class="h-full w-full relative">
-                <video preload="metadata" src="@/assets/videos/02-Yapci-monumenta.mp4" muted autoplay loop playsinline
+                <video preload="metadata" src="/videos/02-Yapci-monumenta.mp4" muted autoplay loop playsinline
                     class="h-full w-full object-cover"></video>
             </div>
         </div>
@@ -16,7 +16,7 @@
         <div v-if="overlayState == 1 || overlayState == 2" class="absolute top-0 w-full h-full bg-black/50 overlayTwo"
             ref="overlayTwo">
             <div class="h-full w-full relative">
-                <video preload="metadata" src="@/assets/videos/03-Yapci-Lloro.mp4" muted autoplay loop playsinline
+                <video preload="metadata" src="/videos/03-Yapci-Lloro.mp4" muted autoplay loop playsinline
                     class="h-full w-full object-cover"></video>
             </div>
         </div>
@@ -25,7 +25,7 @@
         <div v-if="overlayState == 2 || overlayState == 3" class="absolute top-0 w-full h-full bg-black/50 overlay"
             ref="overlayThree">
             <div class="h-full w-full relative">
-                <video preload="metadata" src="@/assets/videos/04-Yapci-Red-Hot.mp4" muted autoplay loop playsinline
+                <video preload="metadata" src="/videos/04-Yapci-Red-Hot.mp4" muted autoplay loop playsinline
                     class="h-full w-full object-cover"></video>
             </div>
         </div>
