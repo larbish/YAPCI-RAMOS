1c2a3c1fb5efd1a409d377df4832f2f94ff9c7e4
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-08-30 12:09:17 +0530

    refactored index

--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,78 @@
 <template>
     <div class="h-[100dvh] w-full relative cursor-none" @click="handleClick">
-        <video preload="metadata" src="/videos/Guayec_30s.mp4" muted autoplay loop class="h-full w-full object-cover"
-            playsinline></video>
+        <video preload="metadata" src="/videos/Guayec_30s.mp4" muted autoplay loop class="h-full w-full object-cover" playsinline></video>
 
-
-        <!-- <img src="~/assets/static/home-background-min.webp" class="h-full w-full object-cover" alt=""> -->
-        <div v-show="overlayState == 0 || overlayState == 1" class="absolute top-0 w-full h-full bg-black/50 overlay"
-            ref="overlay">
+        <div v-show="overlayState === 0 || overlayState === 1" class="absolute top-0 w-full h-full bg-black/50 overlay" ref="overlay">
             <div class="h-full w-full relative">
-                <video preload="metadata" src="/videos/IDontMind_30s.mp4" muted autoplay loop playsinline
-                    class="h-full w-full object-cover"></video>
+                <video preload="metadata" src="/videos/IDontMind_30s.mp4" muted autoplay loop playsinline class="h-full w-full object-cover"></video>
             </div>
         </div>
 
-        <div v-show="overlayState == 1 || overlayState == 2" class="absolute top-0 w-full h-full bg-black/50 overlay"
-            ref="overlayTwo">
+        <div v-show="overlayState === 1 || overlayState === 2" class="absolute top-0 w-full h-full bg-black/50 overlay" ref="overlayTwo">
             <div class="h-full w-full relative">
-                <video preload="metadata" src="/videos/Lloro_30s.mp4" muted autoplay loop playsinline
-                    class="h-full w-full object-cover"></video>
+                <video preload="metadata" src="/videos/Lloro_30s.mp4" muted autoplay loop playsinline class="h-full w-full object-cover"></video>
             </div>
         </div>
 
-
-        <div v-show="overlayState == 2 || overlayState == 3" class="absolute top-0 w-full h-full bg-black/50 overlay"
-            ref="overlayThree">
+        <div v-show="overlayState === 2 || overlayState === 3" class="absolute top-0 w-full h-full bg-black/50 overlay" ref="overlayThree">
             <div class="h-full w-full relative">
-                <video preload="metadata" src="/videos/RedHot_30s.mp4" muted autoplay loop playsinline
-                    class="h-full w-full object-cover"></video>
+                <video preload="metadata" src="/videos/RedHot_30s.mp4" muted autoplay loop playsinline class="h-full w-full object-cover"></video>
             </div>
         </div>
 
-        <div v-show="overlayState == 3 || overlayState == 4" class="absolute top-0 w-full h-full bg-black/50 overlay"
-            ref="overlayFour">
+        <div v-show="overlayState === 3 || overlayState === 4" class="absolute top-0 w-full h-full bg-black/50 overlay" ref="overlayFour">
             <div class="h-full w-full relative">
-                <video preload="metadata" src="/videos/Guayec_30s.mp4" muted autoplay loop playsinline
-                    class="h-full w-full object-cover"></video>
+                <video preload="metadata" src="/videos/Guayec_30s.mp4" muted autoplay loop playsinline class="h-full w-full object-cover"></video>
             </div>
         </div>
 
-        <div class="w-full h-fit absolute bottom-0 z-20 text-center mix-blend-difference" @mouseenter="handleMouseEnter"
-            @mouseleave="handleMouseLeave">
+        <div class="w-full h-fit absolute bottom-0 z-20 text-center mix-blend-difference" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
             <div class="w-fit h-fit mx-auto block font-medium" id="disable-hover">
-                <button @click="handleOpenProject" class=" mix-blend-color-burn  p-3 text-white brightness-75">
-                    <span></span>
+                <button @click="handleOpenProject" class="mix-blend-color-burn p-3 text-white brightness-75">
                     PARTO DE MÍ. 2022. AUDIOVISUAL
                 </button>
             </div>
-
         </div>
     </div>
 </template>
 
-
 <script setup>
-const overlay = ref(null)
-const overlayTwo = ref(null)
-const overlayThree = ref(null)
-const overlayFour = ref(null)
-const overlayState = ref(0)
-const moveAnimationEnabled = useState('moveAnimationEnabled')
+const overlay = ref(null);
+const overlayTwo = ref(null);
+const overlayThree = ref(null);
+const overlayFour = ref(null);
+const overlayState = ref(0);
+const moveAnimationEnabled = ref(true);
 
 const handleMouseEnter = () => {
     moveAnimationEnabled.value = false;
-}
+};
 
 const handleMouseLeave = () => {
-    moveAnimationEnabled.value = true
-}
+    moveAnimationEnabled.value = true;
+};
 
 const handleMove = (x, y) => {
-    if (overlayState.value == 0) {
-        useUpdateClipPath(x, y, overlay, moveAnimationEnabled.value ? 'move' : 'disable');
-    } else if (overlayState.value == 1) {
-        useUpdateClipPath(x, y, overlayTwo, moveAnimationEnabled.value ? 'move' : 'disable');
-    } else if (overlayState.value == 2) {
-        useUpdateClipPath(x, y, overlayThree, moveAnimationEnabled.value ? 'move' : 'disable');
-    } else if (overlayState.value == 3) {
-        useUpdateClipPath(x, y, overlayFour, moveAnimationEnabled.value ? 'move' : 'disable');
-    }
-}
+    const currentOverlay = [overlay, overlayTwo, overlayThree, overlayFour][overlayState.value];
+    const action = moveAnimationEnabled.value ? 'move' : 'disable';
+    useUpdateClipPath(x, y, currentOverlay, action);
+};
 
 const handleMouseMove = (e) => handleMove(Math.round(e.clientX / window.innerWidth * 100), Math.round(e.clientY / window.innerHeight * 100));
 const handleTouchMove = (e) => handleMove(Math.round(e.touches[0].clientX / window.innerWidth * 100), Math.round(e.touches[0].clientY / window.innerHeight * 100));
 
-
-const handleTouchEnd = (e) => {
-    console.log('end')
-
-    if (overlayState.value == 0) {
-        useUpdateClipPath(50, 50, overlay, 'disable')
-    }
-    if (overlayState.value == 1) {
-        useUpdateClipPath(50, 50, overlayTwo, 'disable')
-    }
-    if (overlayState.value == 2) {
-        useUpdateClipPath(50, 50, overlayThree, 'disable')
-    }
-    if (overlayState.value == 3) {
-        useUpdateClipPath(50, 50, overlayFour, 'disable')
-    }
-}
+const handleTouchEnd = () => {
+    const currentOverlay = [overlay, overlayTwo, overlayThree, overlayFour][overlayState.value];
+    useUpdateClipPath(50, 50, currentOverlay, 'disable');
+};
 
 const checkMobile = () => {
     if (window.matchMedia("(max-width: 767px)").matches) {
-        // Apply mobile-specific behavior
         handleTouchEnd();
     }
-}
+};
 
 onMounted(() => {
-    moveAnimationEnabled.value = true;
     checkMobile();
     window.addEventListener('touchmove', handleTouchMove);
     window.addEventListener('mousemove', handleMouseMove);
@@ -123,33 +86,33 @@ onBeforeUnmount(() => {
 });
 
 const handleClick = async (e) => {
-    if (!moveAnimationEnabled.value) return null
-    const { clientX, clientY } = e
-    const x = Math.round(clientX / window.innerWidth * 100)
-    const y = Math.round(clientY / window.innerHeight * 100)
+    if (!moveAnimationEnabled.value) return;
 
+    const { clientX, clientY } = e;
+    const x = Math.round(clientX / window.innerWidth * 100);
+    const y = Math.round(clientY / window.innerHeight * 100);
 
-    overlayState.value = overlayState.value + 1
+    overlayState.value = (overlayState.value + 1) % 5;
+    const overlayRefs = [overlay, overlayTwo, overlayThree, overlayFour];
+    const isMobile = window.matchMedia("(max-width: 767px)").matches;
 
-
-
-    if (overlayState.value == 1) useUpdateClipPath(x, y, overlay, 'click')
-
-    if (overlayState.value == 2) useUpdateClipPath(x, y, overlayTwo, 'click')
-
-    if (overlayState.value == 3) useUpdateClipPath(x, y, overlayThree, 'click')
-
-    if (overlayState.value == 4) await useUpdateClipPath(x, y, overlayFour, 'click')
-
-    if (overlayState.value >= 4) {
-        console.log('over 4')
-        overlayState.value = 0
+    if (overlayState.value === 4) {
+        console.log('over 4');
+        overlayState.value = 0;
+        console.log(overlayState.value);
     }
-}
 
+    overlayRefs.forEach((ref, index) => {
+        if (index === overlayState.value - 1 || index === overlayState.value) {
+            useUpdateClipPath(x, y, ref, overlayState.value === index + 1 ? 'click' : 'move');
+        }
+    });
 
+    if (!isMobile && overlayState.value >= 4) {
+        overlayRefs.forEach(ref => useUpdateClipPath(50, 50, ref, 'move'));
+    }
+};
 </script>
-
 
 <style>
 .overlay {
