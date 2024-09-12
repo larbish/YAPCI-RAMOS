9d1b3469aae1d67e557db136f3d8a636061179aa
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-09-12 16:09:55 +0700

    chore(app): initial commit by Nuxt Studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  ssr: true,
-  css: ['~/assets/css/main.css', '~/assets/fonts/main.css'],
-  modules: ['@formkit/auto-animate/nuxt'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  experimental: {
-    typedPages: true
+  devtools: { enabled: true },
+  modules: ['@nuxt/content'],
+  routeRules: {
+    '/': { prerender: true }
   }
 })
