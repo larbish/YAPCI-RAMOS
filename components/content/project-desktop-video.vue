3ecdfef08541a8dcef92d5b1aac73e442bbab32c
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-09-25 05:29:23 +0530

    Update project-desktop-video.vue

---
+++ components/content/project-desktop-video.vue
@@ -0,0 +1,50 @@
+<template>
+    <div v-if="props.hasText === 'true'" :style="style">
+        <video :src="props.videoPath" class="w-full pointer-events-auto" muted loop autoplay playsinline></video>
+        <p class="uppercase">{{ props.textContent }}</p>
+    </div>
+
+    <div v-else :style="noTextStyle">
+        <video :style="`width: ${props.childWidth}`" :src="props.videoPath" class="pointer-events-auto" muted loop
+            autoplay playsinline></video>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    videoPath: String,
+    imageAlt: String,
+    hasText: String,
+    textContent: String,
+    marginLeft: String,
+    flexDirection: String,
+    childWidth: {
+        type: String,
+        default: '100%'
+    },
+    width: {
+        type: String,
+        default: '100%'
+    },
+    justifyContent: {
+        type: String,
+        default: 'flex-start', // Default value if none provided
+    },
+    alignItems: {
+        type: String,
+        default: 'flex-start', // Optional: You can set a default alignment
+    },
+})
+
+const style = computed(() => ({
+    marginLeft: props.marginLeft,
+    width: props.width,
+}))
+
+const noTextStyle = computed(() => ({
+    marginLeft: props.marginLeft,
+    display: 'flex',
+    justifyContent: props.justifyContent,
+    alignItems: props.alignItems,
+}))
+</script>
