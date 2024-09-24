720935c1ac32f96e3f8839c5f2bebec9312a02f0
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-09-25 01:56:56 +0530

    Update project-mobile-text-wrapper.vue

--- components/content/project-mobile-text-wrapper.vue
+++ components/content/project-mobile-text-wrapper.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="props.hasTitle == 'true'" class="w-full flex items-start">
+    <div v-if="props.hasTitle == 'true'" class="w-full flex items-start text-black">
         <div :style="`width: ${props.width}`" class="p-2 flex gap-x-2 leading-[1.1]">
             <h2 class=" uppercase font-medium">
                 {{ props.title }}
