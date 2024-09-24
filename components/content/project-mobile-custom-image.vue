0304ba1cd909dcc59ae8da0cfb4ec18f51c110cd
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-09-25 02:47:30 +0530

    Create project-mobile-custom-image.vue

---
+++ components/content/project-mobile-custom-image.vue
@@ -0,0 +1,13 @@
+<template>
+        <img :src="imagePath" class="h-full min-w-fit w-fit max-w-fit" :style="{ height: props.height }" alt="">
+</template>
+
+<script setup>
+const props = defineProps({
+    imagePath: String,
+    height: {
+        type: String,
+        default: 'fit'
+    }
+})
+</script>
