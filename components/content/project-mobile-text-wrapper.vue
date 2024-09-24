e12127ac181241cd30d42fb519cf18d7c3f5740d
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-09-25 01:44:20 +0530

    Create project-mobile-text-wrapper.vue

---
+++ components/content/project-mobile-text-wrapper.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="props.hasTitle == 'true'" class="w-full flex items-start">
+        <div :style="`width: ${props.width}`" class="p-2 flex gap-x-2 leading-[1.1]">
+            <h2 class=" uppercase font-medium">
+                {{ props.title }}
+            </h2>
+            <div class="flex flex-col ml-auto w-[63.2%] min-w-[63.2%] max-w-[63.2%] gap-y-3">
+                <ContentSlot />
+            </div>
+        </div>
+    </div>
+
+    <div v-else class="w-full p-1 lg:pl-2">
+        <div :style="`width: ${props.width}`">
+            <ContentSlot />
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    hasTitle: String,
+    title: String,
+    width: {
+        type: String,
+        default: '100%'
+    }
+})
+</script>
