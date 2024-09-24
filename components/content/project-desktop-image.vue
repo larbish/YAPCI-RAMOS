c403bc66f84e88cc8e4967b63f5c71f805ccfe83
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-09-24 19:41:21 +0530

    Update project-desktop-image.vue

--- components/content/project-desktop-image.vue
+++ components/content/project-desktop-image.vue
@@ -4,7 +4,7 @@
         <p class="uppercase">{{ props.textContent }}</p>
     </div>
 
-    <div v-else class="w-full" :style="style" >
+    <div v-else class="w-full" :style="noTextStyle" >
         <img class="w-full" :src="props.imagePath" :alt="props.imageAlt">
     </div>
 </template>
@@ -15,10 +15,26 @@ const props = defineProps({
     imageAlt: String,
     hasText: String,
     textContent: String,
-    marginLeft: String
+    marginLeft: String,
+    flexDirection: String,
+    justifyContent: {
+        type: String,
+        default: 'flex-start', // Default value if none provided
+    },
+    alignItems: {
+        type: String,
+        default: 'flex-start', // Optional: You can set a default alignment
+    },
 })
 
 const style = computed(() => ({
-    marginLeft: props.marginLeft
+    marginLeft: props.marginLeft,
+}))
+
+const noTextStyle = computed(() => ({
+    marginLeft: props.marginLeft,
+    display: 'flex',
+    justifyContent: props.justifyContent,
+    alignItems: props.alignItems,
 }))
 </script>
