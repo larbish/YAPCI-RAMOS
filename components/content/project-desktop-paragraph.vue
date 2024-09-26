148225fe814c0fce122710308a2fba2a5ec95285
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-09-26 12:11:35 +0530

    Update project-desktop-paragraph.vue

--- components/content/project-desktop-paragraph.vue
+++ components/content/project-desktop-paragraph.vue
@@ -1,48 +1,11 @@
 <template>
-    <div v-if="props.hasText === 'true'" :style="style">
-        <video :src="props.videoPath" class="w-full pointer-events-auto" muted loop autoplay playsinline></video>
-        <p class="uppercase">{{ props.textContent }}</p>
-    </div>
-
-    <div v-else :style="noTextStyle" >
-        <video :src="props.videoPath" :style="`width: ${props.childWidth}`" class="pointer-events-auto" muted loop autoplay playsinline></video>
+    <div class="w-full">
+        {{ props.content }}
     </div>
 </template>
 
 <script setup>
 const props = defineProps({
-    videoPath: String,
-    imageAlt: String,
-    hasText: String,
-    textContent: String,
-    marginLeft: String,
-    flexDirection: String,
-    childWidth: {
-        type: String,
-        default: '100%'
-    },
-    width:{
-        type: String,
-        default: '100%'
-    },
-    justifyContent: {
-        type: String,
-        default: 'flex-start', // Default value if none provided
-    },
-    alignItems: {
-        type: String,
-        default: 'flex-start', // Optional: You can set a default alignment
-    },
+    content: String
 })
-
-const style = computed(() => ({
-    marginLeft: props.marginLeft,
-}))
-
-const noTextStyle = computed(() => ({
-    marginLeft: props.marginLeft,
-    display: 'flex',
-    justifyContent: props.justifyContent,
-    alignItems: props.alignItems,
-}))
 </script>
