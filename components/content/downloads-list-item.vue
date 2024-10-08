a06e7ab12dbcea7cdf3d77f2150d67c53af3eba4
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-10-08 15:18:11 +0530

    Update downloads-list-item.vue

--- components/content/downloads-list-item.vue
+++ components/content/downloads-list-item.vue
@@ -1,7 +1,7 @@
 <template>
     <a target="_blank" :href="link"
-        class="w-[90vw] sm:w-[40vw] 2xl:w-[30vw] gap-x-4 grid grid-cols-3 !leading-[1.1] downloads-list-item">
-        <p class="col-span-2 leading-[1.1]">{{ props.title }}</p>
+        class="w-[90vw] sm:w-[30vw] 2xl:w-[20vw] gap-x-4 grid grid-cols-2 !leading-[1.1] downloads-list-item">
+        <p class=" leading-[1.1]">{{ props.title }}</p>
         <p>{{ props.author }}</p>
     </a>
 </template>
