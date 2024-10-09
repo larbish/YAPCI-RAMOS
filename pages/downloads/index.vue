10b1247fb1ba7421e9388a4225ccd93a6c6cdb05
tejabharathhq <165658296+tejabharathhq@users.noreply.github.com>
2024-10-09 09:28:41 +0530

    update

---
+++ pages/downloads/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <ContentRenderer :value="data" />
+</template>
+
+<script setup>
+const route = useRoute();
+
+const { data } = await useAsyncData(`content-${route.params.category}`, () => {
+    return queryContent('downloads', '').findOne()
+})
+</script>
